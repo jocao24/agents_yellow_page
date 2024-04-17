8f80a123321e3132f4a3d38970bdcc1bf5f1b173
Deiber Olivasres <deiber.olivares@infinitytech.dev>
2024-04-16 21:26:04 -0500

    Fix agents available

--- src/network/service_discovery.py
+++ src/network/service_discovery.py
@@ -29,34 +29,46 @@ def send_list_agents(agents: dict, keys_asimetrics: dict, nameserver: Pyro4.Prox
     management_logs.log_message("ServiceDiscovery -> Sending list of agents")
     try:
         if agents != {} and len(agents) > 1:
-            list_agents_dict = {}
-            for k, v in agents.items():
-                list_agents_dict[v['id']] = {
-                    'name': v['name'],
-                    'description': v['description'],
-                    'skills': v['skills'],
-                    'public_key': v['public_key'],
-                    'id': v['id'],
-                    'is_client': v['is_client']
+            for k, current_agent in agents.items():
+                # Crear un nuevo diccionario que incluye todos los agentes excepto el actual (current_agent['id'])
+                list_agents_dict = {
+                    agent_id: {
+                        'name': details['name'],
+                        'description': details['description'],
+                        'skills': details['skills'],
+                        'public_key': details['public_key'],
+                        'id': details['id'],
+                        'is_client': details['is_client']
+                    }
+                    for agent_id, details in agents.items() if agent_id != current_agent['id']
                 }
-            management_logs.log_message("ServiceDiscovery -> List of agents uploaded successfully")
-            management_logs.log_message("ServiceDiscovery -> Sending list of agents to all agents...")
 
-            for k, v in agents.items():
-                public_key_agent = keys_asimetrics[v['id']]["public_key_agent"]
+                public_key_agent = keys_asimetrics[current_agent['id']]["public_key_agent"]
                 try:
-                    management_logs.log_message(f"ServiceDiscovery -> Sending list of agents to agent {v['id']}")
-                    management_logs.log_message(f"ServiceDiscovery -> The directory to be sent to the agent {v['id']} is encrypted using hybrid encryption")
-                    data_encrypted = encrypt_data_with_public_key(public_key_agent, list_agents_dict, k, management_logs)
-                    management_logs.log_message(f"ServiceDiscovery -> Data encrypted successfully for agent {v['id']}")
-                    agent_uri = locate_agent(v['id'], nameserver, management_logs)
-                    management_logs.log_message(f"ServiceDiscovery -> Agent {v['id']} located at {agent_uri}")
+                    management_logs.log_message(
+                        f"ServiceDiscovery -> Sending list of agents to agent {current_agent['id']}")
+                    management_logs.log_message(
+                        f"ServiceDiscovery -> The directory to be sent to the agent {current_agent['id']} is encrypted using hybrid encryption")
+
+                    # Encrypt data excluding the current agent
+                    data_encrypted = encrypt_data_with_public_key(public_key_agent, list_agents_dict,
+                                                                  current_agent['id'], management_logs)
+                    management_logs.log_message(
+                        f"ServiceDiscovery -> Data encrypted successfully for agent {current_agent['id']}")
+
+                    agent_uri = locate_agent(current_agent['id'], nameserver, management_logs)
+                    management_logs.log_message(
+                        f"ServiceDiscovery -> Agent {current_agent['id']} located at {agent_uri}")
+
                     agent_proxy = Pyro4.Proxy(agent_uri)
-                    management_logs.log_message(f"ServiceDiscovery -> Proxy created for agent {v['id']}")
-                    management_logs.log_message(f"ServiceDiscovery -> Sending list of agents to agent {v['id']}")
+                    management_logs.log_message(f"ServiceDiscovery -> Proxy created for agent {current_agent['id']}")
+                    management_logs.log_message(
+                        f"ServiceDiscovery -> Sending list of agents to agent {current_agent['id']}")
+
                     agent_proxy.receive_list_agents(data_encrypted)
                 except Exception as e:
-                    management_logs.log_message(f"ServiceDiscovery -> Error sending list of agents to agent {v['id']}: {e}")
+                    management_logs.log_message(
+                        f"ServiceDiscovery -> Error sending list of agents to agent {current_agent['id']}: {e}")
                     continue
 
     except Exception as e:
